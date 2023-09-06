82f522c4826b7de5d0409c1f7996120f49c5ca3a
Paulo-Evo <rm89569@estudante.fieb.edu.br>
2023-09-05 22:12:12 -0300

    Começo do crud tcc 05/09

---
+++ src/main/java/com/itb/tcc/evobody/evo/model/Usuario.java
@@ -0,0 +1,56 @@
+package com.itb.tcc.evobody.evo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users", uniqueConstraints = UniqueConstraint(columnNames = "email"))
+public class Usuario {
+
+	
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		
+		private Long id;
+		private String firstName;
+		private String lastName;
+		private String email;
+		private String senha;
+		
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getSenha() {
+			return senha;
+		}
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+		
+	
+}
